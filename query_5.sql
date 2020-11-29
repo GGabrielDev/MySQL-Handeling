177fba4386798f09a4ae73dfa214aa8edd5f27db
GGabrielDev <luisgabrielramosrobles@gmail.com>
2020-11-29 11:56:12 -0400

    Added more JOIN related querys

--- query_5.sql
+++ query_5.sql
@@ -13,7 +13,7 @@ SELECT *
 FROM orders o, customers c
 WHERE o.customer_id = c.customer_id;
 
--- Use of OUTER JOIN for pass entries from tables so they don't need to match the ON condition
+-- Use of RIGHT/LEFT (Outer) JOIN for pass entries from tables so they don't need to match the ON condition
 
 SELECT 
 	c.customer_id,
@@ -21,7 +21,8 @@ SELECT
 	o.order_id
 FROM customers c
 LEFT JOIN orders o
-	ON c.customer_id = o.customer_id;
+	ON c.customer_id = o.customer_id
+ORDER BY c.customer_id;
 
 SELECT 
 	p.product_id,
@@ -30,3 +31,32 @@ SELECT
 FROM order_items oi
 RIGHT JOIN products p
 	ON oi.product_id = p.product_id;
+
+-- Outer Joins bettween multiple tables.
+
+SELECT 
+	c.customer_id,
+	c.first_name,
+	o.order_id,
+	sh.name AS shipper
+FROM customers c
+LEFT JOIN orders o
+	ON c.customer_id = o.customer_id
+LEFT JOIN shippers sh
+	ON o.shipper_id = sh.shipper_id
+ORDER BY c.customer_id;
+
+SELECT 
+	o.order_date,
+	o.order_id,
+	c.first_name AS customer,
+	s.name AS shipper,
+	os.name AS status
+FROM orders o
+JOIN customers c
+	ON c.customer_id = o.customer_id
+LEFT JOIN shippers s
+	ON o.shipper_id = s.shipper_id
+LEFT JOIN order_statuses os
+	ON o.status = os.order_status_id
+ORDER BY o.order_id;
